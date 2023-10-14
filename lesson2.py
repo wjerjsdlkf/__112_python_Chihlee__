7a6a5d69ceabc033ed5c53f1183c3a3821482a5b
wjerjsdlkf <147892159+wjerjsdlkf@users.noreply.github.com>
2023-10-14 08:13:36 +0000

    第一次傳回

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+side = int(input('請輸入對邊：'))
+another_side = int(input('請輸入斜邊：'))
+
+import math
+radian = math.asin(side/another_side) #tuple  asin計算弧度
+degree = math.degrees(radian) #degrees 弧度轉角度
+print(round(degree,ndigits=2)) #round 4捨5入 ndigits 小數點幾位
